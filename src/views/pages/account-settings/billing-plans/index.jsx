d3ff6f12c93a52ec55fe65d00ef67bfdc8e7b038
Chandru-genpluse <genplusdev6@gmail.com>
2025-10-10 16:34:28 +0530

    login clear

---
+++ src/views/pages/account-settings/billing-plans/index.jsx
@@ -0,0 +1,4 @@
+// Reuse billing components from user view to populate the Account Settings billing tab
+import BillingPlans from '@views/apps/user/view/user-right/billing-plans'
+
+export default BillingPlans
